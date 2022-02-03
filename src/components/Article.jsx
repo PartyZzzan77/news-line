65a522368bc37f16c092c389245bde83ec3cfa95
Mitya <88058465+PartyZzzan77@users.noreply.github.com>
2022-02-03 21:07:13 +0300

    refactoring Article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -17,7 +17,7 @@ class Article extends Component {
         <p className='news__author'>{author}:</p>
         <p className='news__text'>{text}</p>
         {
-          !visible && <a onClick={this.handleReadMoreClck} href="#" className='news__readmore'>Подробнее</a>
+          !visible && <a onClick={this.handleReadMoreClck} href="#anywere" className='news__readmore'>Подробнее</a>
         }
         {
           visible && <p className='news__big-text'>{bigText}</p>
